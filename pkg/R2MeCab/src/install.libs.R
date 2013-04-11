119e4074731fff4eb1d32584baf6997aeeb86a8b
ishida (Prof ) <ishida-m@ias.tokushima-u.ac.jp>
2013-04-11 03:42:35 +0000

    install.libs.R revised
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r2mecab@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R2MeCab/src/install.libs.R
+++ pkg/R2MeCab/src/install.libs.R
@@ -2,30 +2,35 @@
 
 if (WINDOWS) {
   files <- Sys.glob(paste("*", SHLIB_EXT, sep=''))
-  # cat ("files = ", files, "\n")
+   cat ("files = ", files, "\n")
   libarch <- if (nzchar(R_ARCH)) paste('libs', R_ARCH, sep='') else 'libs'
-  # cat ("R_ARCH = ", R_ARCH, "\n")
-  # cat ("libarch = ", libarch, "\n")
+  
+   cat ("R_ARCH = ", R_ARCH, "\n")
+   cat ("libarch = ", libarch, "\n")
 
   dest <- file.path(R_PACKAGE_DIR, libarch)
   dest32 <- file.path(R_PACKAGE_DIR, "libs/i386")
-  # cat ("dest32 = ", dest32, "\n")
+   cat ("dest32 = ", dest32, "\n")
 
   dest64 <- file.path(R_PACKAGE_DIR, "libs/x64")
-  # cat ("dest64 = ", dest64, "\n")
+   cat ("dest64 = ", dest64, "\n")
   
   dir.create(dest, recursive = TRUE, showWarnings = FALSE)
   file.copy(files, dest, overwrite = TRUE)
 
-  #if (file.exists ( paste (dest32, "libmecab.dll", sep = "/"))) 
-  #  cat ("found libmecab.dll\n") else cat("no libmecab.dll\n")
+ dir.create(dest64, recursive = TRUE, showWarnings = FALSE)
+  file.copy(files, dest64, overwrite = TRUE)
 
-  if (file.exists ( paste (dest32, "libmecab64.dll", sep = "/"))) 
-    file.remove (paste (dest32, "libmecab64.dll", sep = "/"))
+  if (file.exists ("../inst/libmecab.dll")) 
+    cat ("###################found libmecab.dll\n") else cat("##################no libmecab.dll\n")
 
-  if (file.exists ( paste (dest64, "libmecab.dll", sep = "/")) & file.exists ( paste (dest64, "libmecab64.dll", sep = "/")) ) {
-     file.remove (paste (dest64, "libmecab.dll", sep = "/"))
-                                        file.rename (paste (dest64, "libmecab64.dll", sep = "/") , paste (dest64, "libmecab.dll", sep = "/"))
+  if (file.exists ("../inst/libmecab64.dll")) {
+    file.copy  ("../inst/libmecab64.dll", paste (dest64, "libmecab.dll", sep = "/") )
+    # file.remove ("../inst/libmecab64.dll")
+  }
+  if (file.exists ("../inst/libmecab.dll") ) {
+     file.copy  ("../inst/libmecab.dll", paste (dest, "libmecab.dll", sep = "/") )
+      # file.remove ("../inst/libmecab.dll")
   }
 } else{
   files <- Sys.glob(paste("*", SHLIB_EXT, sep=''))
